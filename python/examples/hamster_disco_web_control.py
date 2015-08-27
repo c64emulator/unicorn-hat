d98a20d04def356b4a4bb58f660c044695480202
Dan Rolfe <dan@titch.org>
2015-08-27 21:44:10 +0000

    Tidy template and add server shutdown capability

--- python/examples/hamster_disco_web_control.py
+++ python/examples/hamster_disco_web_control.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import datetime
 import os
+import sys
 
 app = Flask(__name__)
 
@@ -19,9 +20,22 @@ def hello():
 def kill_hamster_disco():
    try:
       os.remove('/tmp/.hamster_disco')
-      return "Bye, bye hamster disco"
-   except:
-      return "The party was already over"
+      #return "Bye, bye hamster disco"
+   finally:
+      return redirect("/")
+      #return "The party was already over"
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route("/kill_hamster_disco_server")
+def kill_hamster_disco_server():
+   shutdown_server()
+   return redirect("/")
+   #return 'Server shutting down...'
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
